d7029808473c9edea8d817c4cf0c9b5493ea5c68
Daniel Hartnell <danielhartnell@gmail.com>
2018-05-09 16:04:18 -0700

    fixes EC2 security group egress

--- modules/ecs-cluster/network.tf
+++ modules/ecs-cluster/network.tf
@@ -69,6 +69,13 @@ resource "aws_security_group" "ec2-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   timeouts {
     delete = "5m"
   }
