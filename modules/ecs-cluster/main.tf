eb70758a3cbf89d7ec15496c2a5115eca3997573
Daniel Hartnell <danielhartnell@gmail.com>
2018-05-09 13:09:14 -0700

    add EC2 instances to cluster

--- modules/ecs-cluster/main.tf
+++ modules/ecs-cluster/main.tf
@@ -3,3 +3,31 @@
 resource "aws_ecs_cluster" "ecs-cluster" {
   name = "${var.cluster_name}-${var.environment}-cluster"
 }
+
+data "template_file" "user_data" {
+  template = "${file("${path.module}/userdata.sh")}"
+
+  vars {
+    environment  = "${var.environment}"
+    cluster_name = "${var.cluster_name}"
+  }
+}
+
+data "aws_ami" "latest" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn-ami-*-amazon-ecs-optimized"]
+  }
+
+  owners = ["amazon"]
+}
+
+resource "aws_instance" "ecs_instance" {
+  count = "2"
+
+  ami           = "${data.aws_ami.latest.id}"
+  instance_type = "t2.micro"
+  user_data     = "${data.template_file.user_data.rendered}"
+}
---
+++ modules/ecs-cluster/userdata.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo 'ECS_CLUSTER=${cluster_name}-${environment}-cluster' >> /etc/ecs/ecs.config
